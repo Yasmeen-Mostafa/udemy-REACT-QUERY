378a7723a3f0444f4de2cc6cbf1f37f3e9488508
Yasmeen Mostafa <yasmeen.mostafa1236@gmail.com>
2024-11-25 08:57:03 +0200

    Updated

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -2,23 +2,40 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
-export function PostDetail({ post }) {
-  // replace with useQuery
-  // const data = [];
-  const {data,isError,error,isLoading} =useQuery({
-    queryKey:['comments',post.id],
-    queryFn:()=>fetchComments(post.id),
-    refetchOnWindowFocus:false
-  })
-  return (
-    isError? <h1>{error}</h1>:isLoading?<h1>Loading ... </h1>:<><h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+export function PostDetail({ post, mutationObj, updatePostMutation }) {
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+    refetchOnWindowFocus: false,
+  });
+  return isError ? (
+    <h1>{error}</h1>
+  ) : isLoading ? (
+    <h1>Loading ... </h1>
+  ) : (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button onClick={() => mutationObj.mutate(post.id)}>Delete</button>
+      {mutationObj.isPending ? (
+        <h1>Pending...</h1>
+      ) : (
+        mutationObj.isSuccess && <h1>Succes oh yeaah</h1>
+      )}
+      <button onClick={() => updatePostMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updatePostMutation.isPending ? (
+        <h1>Pending update...</h1>
+      ) : (
+        updatePostMutation.isSuccess && <h1>Succes update oh yeaah</h1>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
-      ))}</>
+      ))}
+    </>
   );
 }
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,47 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 const maxPostPage = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    refetchOnWindowFocus: false,
+  });
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      let nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
 
-  // replace with useQuery
-  // const data = [];
-  const {data,isFetching}=useQuery({
-    queryKey:['posts',currentPage],
-    queryFn:()=>fetchPosts(currentPage),
-    refetchOnWindowFocus:false
-  })
-  if(!data) return <div></div>
+  const mutatationObj = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+    onSuccess: (newData) => {
+      console.log(newData);
+      queryClient.refetchQueries({ queryKey: ["comments", 7] });
+    },
+  });
+
+  const updatePostMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
+
+  if (!data) return <div></div>;
   return (
     <>
-    {isFetching?<h1>Loading...</h1>:<> <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled={currentPage===1}  onClick={() => setCurrentPage(prev=>prev-1)}>
-          Previous page
-        </button>
-        <span>Page {currentPage}</span>
-        <button disabled={currentPage>=maxPostPage}  onClick={() => setCurrentPage(prev=>prev+1)}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}</>}
-     
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          {" "}
+          <ul>
+            {data.map((post) => (
+              <li
+                key={post.id}
+                className="post-title"
+                onClick={() => {
+                  mutatationObj.reset();
+                  updatePostMutation.reset();
+                  setSelectedPost(post);
+                }}
+              >
+                {post.title}
+              </li>
+            ))}
+          </ul>
+          <div className="pages">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage((prev) => prev - 1)}
+            >
+              Previous page
+            </button>
+            <span>Page {currentPage}</span>
+            <button
+              disabled={currentPage >= maxPostPage}
+              onClick={() => setCurrentPage((prev) => prev + 1)}
+            >
+              Next page
+            </button>
+          </div>
+          <hr />
+          {selectedPost && (
+            <PostDetail
+              post={selectedPost}
+              mutationObj={mutatationObj}
+              updatePostMutation={updatePostMutation}
+            />
+          )}
+        </>
+      )}
     </>
   );
 }
